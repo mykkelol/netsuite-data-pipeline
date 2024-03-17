fa38e32a2a93e38ea47d916eab75e9a284a65d8c
mykkelol <1micheal.sihavong2@gmail.com>
2024-03-16 23:11:18 -0700

    remove dynamic task creation in dag to support only dynamic dags

--- dags/dag_netsuite_data_pipeline.py
+++ dags/dag_netsuite_data_pipeline.py
@@ -17,10 +17,16 @@ def dummy_callable(action):
         f'NetSuite GL posting transactions data pipeline'
     )
 
-def create_dag(dag_id, interval, config, search_type, searches):
+def create_dag(
+    dag_id,
+    interval,
+    config,
+    search_type,
+    search_id
+):
     with DAG(
         dag_id=dag_id,
-        description=f'Ingested new GL posting {{search_type}} from NetSuite',
+        description=f'Ingested new GL posting {search_type} from NetSuite',
         schedule_interval=interval,
         start_date=datetime(2024, 2, 25, 2),
         catchup=False
@@ -33,91 +39,73 @@ def create_dag(dag_id, interval, config, search_type, searches):
             dag=dag
         )
 
-        load_netsuite_to_s3_landing = [
-            NetSuiteToS3Operator(
-                task_id=f'extract_{search_id}',
-                search_types=config.SUPPORTED_RECORD_TYPES,
-                search_type=search_type,
-                search_id=search_id,
-                conn_id=config.S3_CONN_ID,
-                bucket_name=config.LANDING_BUCKET,
-                filename='netsuite_extracts_{{ ds_nodash }}',
-                filter_expression=[
-                    ['taxline','is','F'], 
-                    'AND', 
-                    ['cogs','is','F'], 
-                    'AND', 
-                    ['posting','is','T'], 
-                    'AND', 
-                    ['accountingbook','anyof',config.ACCOUNTING_BOOKS['secondary']]
-                ],
-                columns=None,
-                dag=dag
-            )
-            for search_id in searches
-        ]
+        load_netsuite_to_s3_landing = NetSuiteToS3Operator(
+            task_id=f'extract_{search_id}',
+            search_types=config.SUPPORTED_RECORD_TYPES,
+            search_type=search_type,
+            search_id=search_id,
+            conn_id=config.S3_CONN_ID,
+            bucket_name=config.LANDING_BUCKET,
+            filename='netsuite_extracts_{{ ds_nodash }}',
+            filter_expression=[
+                ['taxline','is','F'], 
+                'AND', 
+                ['cogs','is','F'], 
+                'AND', 
+                ['posting','is','T'], 
+                'AND', 
+                ['accountingbook','anyof',config.ACCOUNTING_BOOKS['secondary']]
+            ],
+            columns=None,
+            dag=dag
+        )
 
-        truncate_postgres_staging = [ 
-            PostgresOperator(
-                task_id=f'truncate_postgres_{search_id}',
-                postgres_conn_id=config.POSTGRES_CONN_ID,
-                sql=f'TRUNCATE TABLE stage_{search_id}',
-            )
-            for search_id in searches
-        ]
+        truncate_postgres_staging = PostgresOperator(
+            task_id=f'truncate_postgres_{search_id}',
+            postgres_conn_id=config.POSTGRES_CONN_ID,
+            sql=f'TRUNCATE TABLE stage_{search_id}',
+        )
 
-        load_s3_landing_to_postgres_staging = [ 
-            S3ToPostgresTransferOperator(
-                task_id=f'load_to_postgres_stage_{search_id}',
-                aws_conn_id=config.S3_CONN_ID,
-                s3_key='netsuite_extracts_{{ ds_nodash }}.csv',
-                s3_bucket_name=config.LANDING_BUCKET,
-                postgres_conn_id=config.POSTGRES_CONN_ID,
-                postgres_table=f'stage_{search_id}',
-                dag=dag
-            )
-            for search_id in searches
-        ]
+        load_s3_landing_to_postgres_staging = S3ToPostgresTransferOperator(
+            task_id=f'load_to_postgres_stage_{search_id}',
+            aws_conn_id=config.S3_CONN_ID,
+            s3_key='netsuite_extracts_{{ ds_nodash }}.csv',
+            s3_bucket_name=config.LANDING_BUCKET,
+            postgres_conn_id=config.POSTGRES_CONN_ID,
+            postgres_table=f'stage_{search_id}',
+            dag=dag
+        )
 
-        load_postgres_staging_to_final = [
-            PostgresOperator(
-                task_id=f'load_postgres_final_{search_id}',
-                postgres_conn_id=config.POSTGRES_CONN_ID,
-                sql=f"""
-                    INSERT INTO final_{search_id}
-                    SELECT *
-                    FROM stage_{search_id} AS stage
-                    WHERE stage.id IS NOT NULL AND NOT EXISTS (
-                        SELECT 1
-                        FROM final_{search_id} AS final
-                        WHERE final.id = stage.id
-                    )
-                """
-            )
-            for search_id in searches
-        ]
+        load_postgres_staging_to_final = PostgresOperator(
+            task_id=f'load_postgres_final_{search_id}',
+            postgres_conn_id=config.POSTGRES_CONN_ID,
+            sql=f"""
+                INSERT INTO final_{search_id}
+                SELECT *
+                FROM stage_{search_id} AS stage
+                WHERE stage.id IS NOT NULL AND NOT EXISTS (
+                    SELECT 1
+                    FROM final_{search_id} AS final
+                    WHERE final.id = stage.id
+                )
+            """
+        )
 
-        load_s3_landing_to_lake = [
-            S3CopyObjectOperator(
-                task_id=f'load_s3_lake_{search_id}',
-                aws_conn_id=config.S3_CONN_ID,
-                source_bucket_key='S3://%s/netsuite_extracts_{{ ds_nodash }}.csv' % config.LANDING_BUCKET,
-                dest_bucket_key='S3://%s/netsuite_extracts_{{ ds_nodash }}.csv' % config.LAKE_BUCKET,
-                dag=dag,
-            )
-            for search_id in searches
-        ]
+        load_s3_landing_to_lake = S3CopyObjectOperator(
+            task_id=f'load_s3_lake_{search_id}',
+            aws_conn_id=config.S3_CONN_ID,
+            source_bucket_key='S3://%s/netsuite_extracts_{{ ds_nodash }}.csv' % config.LANDING_BUCKET,
+            dest_bucket_key='S3://%s/netsuite_extracts_{{ ds_nodash }}.csv' % config.LAKE_BUCKET,
+            dag=dag,
+        )
 
-        delete_s3_landing = [
-            S3DeleteObjectsOperator(
-                task_id=f'delete_s3_landing_{search_id}',
-                aws_conn_id=config.S3_CONN_ID,
-                bucket=config.LANDING_BUCKET,
-                keys='netsuite_extracts_{{ ds_nodash }}.csv',
-                dag=dag,
-            )
-            for search_id in searches
-        ]
+        delete_s3_landing = S3DeleteObjectsOperator(
+            task_id=f'delete_s3_landing_{search_id}',
+            aws_conn_id=config.S3_CONN_ID,
+            bucket=config.LANDING_BUCKET,
+            keys='netsuite_extracts_{{ ds_nodash }}.csv',
+            dag=dag,
+        )
         
         finish = PythonOperator(
             task_id='finishing_pipeline',
@@ -125,24 +113,27 @@ def create_dag(dag_id, interval, config, search_type, searches):
             op_kwargs={'action': 'finishing'},
         )
         
-        chain(
-            start,
-            load_netsuite_to_s3_landing,
-            truncate_postgres_staging,
-            load_s3_landing_to_postgres_staging,
-            load_postgres_staging_to_final,
-            load_s3_landing_to_lake,
-            delete_s3_landing,
-            finish
+        (
+            start
+            >> load_netsuite_to_s3_landing
+            >> truncate_postgres_staging
+            >> load_s3_landing_to_postgres_staging
+            >> [
+                load_postgres_staging_to_final,
+                load_s3_landing_to_lake
+            ]
+            >> delete_s3_landing
+            >> finish
         )
             
     return dag
 
-if len(config.TRANSACTION_TYPES) > 5:
+if len(config.RECORD_TYPES) > 5:
     raise ValueError('Exceeds NetSuite SuiteScript usage governance limit')
 
-for i, item in enumerate(config.TRANSACTION_TYPES.items()):
-    search_type, searches = item
+for i, type in enumerate(config.RECORD_TYPES):
+    search_type = type['type']
+    search_id = type.get('search_id')
     dag_id = f'netsuite_data_pipeline_{search_type}'
     interval = f'{str(i * 10)} * * * *'
 
@@ -151,5 +142,5 @@ for i, item in enumerate(config.TRANSACTION_TYPES.items()):
         interval,
         config,
         search_type,
-        searches
+        search_id
     )
--- dags/dags_config.py
+++ dags/dags_config.py
@@ -1,4 +1,40 @@
 class Config:
+    SUPPORTED_RECORD_TYPES = [
+        'transaction',
+        'invoice',
+        'vendorbill',
+        'journalentry',
+        'creditmemo',
+        'payment',
+        'customerpayment',
+        'customer',
+        'vendor',
+        'class',
+        'department',
+        'subsidiary',
+        'currency',
+    ]
+
+    RECORD_TYPES = [
+        {
+            'type': 'transaction',
+            'search_id': 'customsearch_gl_posting_transactions_india',
+        }
+    ]
+
+    ACCOUNTING_BOOKS = {
+        'primary': 0,
+        'secondary': 3
+    }
+
+    S3_CONFIG = {
+        'host': 'host.docker.internal',
+        'port': '5432',
+        'db': 0
+    }
+
+    S3_CONN_ID = 'minio_conn'
+
     POSTGRES_CONFIG = {
         'host': 'host.docker.internal',
         'port': '5432',
@@ -6,41 +42,6 @@ class Config:
     }
 
     POSTGRES_CONN_ID = 'postgres_localhost'
-
-    SUPPORTED_RECORD_TYPES = [
-        'transaction',
-        'customer',
-        'class',
-        'department',
-        'subsidiary',
-        'currency',
-    ]
-
-    TRANSACTION_TYPES = {
-        'transaction': [
-            'customsearch_gl_posting_transactions_india',
-        ]
-    }
-
-    ACCOUNTING_BOOKS = {
-        'primary': 0,
-        'secondary': 3
-    }
-
-    VALIDATOR_CONFIG = {
-        'description_length': 10,
-        'languages': [
-            'en', 'pl', 'es', 'de'
-        ]
-    }
-
-    S3_CONFIG = {
-        "host": "host.docker.internal",
-        "port": "5432",
-        "db": 0
-    }
-
-    S3_CONN_ID = "minio_conn"
 
     LANDING_BUCKET = 'finance-data-lake-landing'
     LAKE_BUCKET = 'finance-data-lake'
